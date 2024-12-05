8d238dd781a55cedc4ca97732500fc17c547998c
J0seJuli0 <josejuliosanchezcruzado1@gmail.com>
2024-12-05 14:36:56 -0500

    Actualizando el servicios de MenuService ApiPermisosMichus

---
+++ apiPermisosMichus/src/main/java/com/permisos/service/MenuService.java
@@ -0,0 +1,25 @@
+package com.permisos.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.permisos.model.entity.Menu;
+import com.permisos.model.repository.IMenuRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class MenuService {
+
+	private IMenuRepository menuRepository;
+	
+	public Optional<Menu> obtenerPorId(Long idMenu){
+		return menuRepository.findById(idMenu);
+	}
+	
+	public Menu guardar(Menu menu) {
+		return menuRepository.save(menu);
+	}
+}
