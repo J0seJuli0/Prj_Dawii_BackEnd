3f538031a9e7e43768c105e272fae30843238525
J0seJuli0 <josejuliosanchezcruzado1@gmail.com>
2024-12-04 20:25:59 -0500

    Actualizando el repositorio del PermisosRepository ApiMichusPermisos

--- apiPermisosMichus/src/main/java/com/permisos/model/repository/IPermisoRolRepository.java
+++ apiPermisosMichus/src/main/java/com/permisos/model/repository/IPermisoRolRepository.java
@@ -1,9 +1,11 @@
 package com.permisos.model.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.permisos.model.entity.PermisosRol;
+import com.permisos.model.entity.PermisosRolId;
 
-public interface IPermisoRolRepository extends JpaRepository<PermisosRol, String> {
-
+public interface IPermisoRolRepository extends JpaRepository<PermisosRol, PermisosRolId> {
+	List<PermisosRol> findByIdIdRol(String idRol);
 }
