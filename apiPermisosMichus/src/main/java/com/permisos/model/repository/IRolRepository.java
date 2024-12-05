6c60b61f1505670aff8e3231be5985d6e011b319
J0seJuli0 <josejuliosanchezcruzado1@gmail.com>
2024-12-04 20:26:34 -0500

    Actualizando el repositorio del RolRepository ApiMichusPermisos

--- apiPermisosMichus/src/main/java/com/permisos/model/repository/IRolRepository.java
+++ apiPermisosMichus/src/main/java/com/permisos/model/repository/IRolRepository.java
@@ -1,9 +1,12 @@
 package com.permisos.model.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.permisos.model.entity.Rol;
 
 public interface IRolRepository extends JpaRepository<Rol, String> {
+
+
 
 }
