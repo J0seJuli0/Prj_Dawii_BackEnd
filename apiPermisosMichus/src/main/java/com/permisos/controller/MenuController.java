eab92857ef51eed7c39f7a51e0c22f8051f4d18c
J0seJuli0 <josejuliosanchezcruzado1@gmail.com>
2024-12-05 14:35:19 -0500

    Actualizando el controlador de MenuController ApiPermisosMichus

--- apiPermisosMichus/src/main/java/com/permisos/controller/MenuController.java
+++ apiPermisosMichus/src/main/java/com/permisos/controller/MenuController.java
@@ -1,13 +1,19 @@
 package com.permisos.controller;
 
 import java.util.List;
-
+	import java.util.Optional;
+	
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.permisos.model.entity.Menu;
 import com.permisos.model.repository.IMenuRepository;
+import com.permisos.service.MenuService;
 
 import lombok.AllArgsConstructor;
 
@@ -17,9 +23,12 @@ import lombok.AllArgsConstructor;
 public class MenuController {
 
 	private IMenuRepository menuRepository;
-	
+	private MenuService menuService;
 	@GetMapping("/listar")
 	public List<Menu> listar(){
 		return menuRepository.findAll();
 	}
+	
+	
+
 }
