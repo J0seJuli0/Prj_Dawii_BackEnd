68629266b51e76112f704592622a0caad7a09a53
MauriciOjeda <mauricioojeda0204@gmail.com>
2024-12-05 00:52:41 -0500

    ACTUALIZACION DE SERVICIOS IMPLEMENT

--- apiAdminMichus/src/main/java/com/permisos/service/AdminServiceImpl.java
+++ apiAdminMichus/src/main/java/com/permisos/service/AdminServiceImpl.java
@@ -1,6 +1,7 @@
 package com.permisos.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -66,5 +67,11 @@ public class AdminServiceImpl implements AdminService{
 		
 		return adminFeign.listarUsuariosSistema();
 	}
+	
+	
+	@Override
+    public Map<String, Object> login(String email, String contrasenia) {
+      return adminFeign.login(email, contrasenia);
+    }
 
 }
