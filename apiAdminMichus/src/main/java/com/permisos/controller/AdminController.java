d546773525a98a636f407b036fda7081fbea5002
MauriciOjeda <mauricioojeda0204@gmail.com>
2024-12-11 18:18:12 -0500

    Actualizar Controlador ADMIN

--- apiAdminMichus/src/main/java/com/permisos/controller/AdminController.java
+++ apiAdminMichus/src/main/java/com/permisos/controller/AdminController.java
@@ -1,10 +1,16 @@
 package com.permisos.controller;
 
 import java.util.List;
+import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.permisos.modal.dto.EmpleadoDTO;
@@ -20,55 +26,89 @@ import com.permisos.service.AdminService;
 @RequestMapping("/admin")
 public class AdminController {
 	
-	@Autowired
-    private AdminService adminService;
+	private final AdminService adminService;
+
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
 	
 	//EMPLEADO -- LISTO
-	@GetMapping("/listarempleado")
+	@GetMapping("/empleado/listarempleado")
     public List<EmpleadoDTO> listarEstudiantes() {
         return adminService.listarEmpleados(); 
     }
 
 	
 	// MENU -- LISTO
-	@GetMapping("/listarmenu")
+	@GetMapping("/menu/listarmenu")
     public List<MenuDTO> listarMenu() {
         return adminService.listarMenu(); 
     }
 	
 	
 	// PERMISOS_ROL -- LISTO
-	@GetMapping("/listarpermisosrol")
+	@GetMapping("/permisosrol/listarpermisorol")
     public List<PermisosRolDTO> listarPermisosRol() {
         return adminService.listarPermisosRol(); 
     }
 	
 	
+	@PostMapping("/permisosrol/crearpermisosrol")
+    public ResponseEntity<String> crearPermisoRol(@RequestBody PermisosRolDTO permisoRol) {
+        return adminService.registrarPermisoRol(permisoRol);
+    }
+	
+	
 	// ROL -- LISTO
-	@GetMapping("/listarrol")
+	@GetMapping("/rol/listarrol")
     public List<RolDTO> listarRol() {
         return adminService.listarRol(); 
     }
 	
+	@PostMapping("/rol/crearrol")
+    public ResponseEntity<RolDTO> crearRol(@RequestBody RolDTO rol) {
+        return adminService.registrarRol(rol);
+    }
+	
+	@PutMapping("/rol/actualizarrol/{idRol}")
+    public ResponseEntity<RolDTO> actualizarRol(@PathVariable("idRol") String idRol, @RequestBody RolDTO rol) {
+        return adminService.actualizarRol(idRol, rol);
+    }
+	
 	
 	// SUB_MENU -- LISTO
-	@GetMapping("/listarsubmenu")
+	@GetMapping("/sub_menu/listarsubmenu")
     public List<SubMenuDTO> listarSubMenu() {
         return adminService.listarSubMenu(); 
     }
 	
+	@PutMapping("/sub_menu/actualizarsubmenu/{idSubMenus}")
+    public ResponseEntity<SubMenuDTO> actualizarSubMenu(@PathVariable("idSubMenus") Long idSubMenus, @RequestBody SubMenuDTO submenu) {
+        return adminService.actualizarSubMenu(idSubMenus, submenu);
+    }
+	
 	
 	// TIPO_DOCUMENTO -- LISTO
-	@GetMapping("/listartipodocumento")
+	@GetMapping("/tipo_documento/listartipodocumento")
     public List<TipoDocumentoDTO> listarTipoDocumento() {
         return adminService.listarTipoDocumento(); 
     }
 	
 	
 	// USUARIOS_SISTEMA -- LISTO
-	@GetMapping("/listarusuariosistema")
+	@GetMapping("/usuarios_sistema/listarusuariosistema")
     public List<UsuariosSistemaDTO> listarUsuariosSistema() {
         return adminService.listarUsuariosSistema(); 
+    }
+
+	
+	
+	// INICIAR SESION:
+	@PostMapping("/login")
+    public ResponseEntity<Map<String, Object>> login(@RequestParam String email, @RequestParam String contrasenia) {
+        Map<String, Object> result = adminService.login(email, contrasenia);
+
+        return ResponseEntity.ok(result);
     }
 	
 }
