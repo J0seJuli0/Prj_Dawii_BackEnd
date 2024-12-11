e54d850080d3ef512bb0de62f5fcb505e16e6848
MauriciOjeda <mauricioojeda0204@gmail.com>
2024-12-11 18:24:13 -0500

    Actualización del Servicio Implement ADMIN

--- apiAdminMichus/src/main/java/com/permisos/service/AdminServiceImpl.java
+++ apiAdminMichus/src/main/java/com/permisos/service/AdminServiceImpl.java
@@ -1,8 +1,10 @@
 package com.permisos.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.permisos.client.AdminFeignClient;
@@ -37,6 +39,12 @@ public class AdminServiceImpl implements AdminService{
 		return adminFeign.listarPermisosRol();
 	}
 		
+	@Override
+	public ResponseEntity<String> registrarPermisoRol(PermisosRolDTO permisoRol) {
+		return adminFeign.registrarPermisoRol(permisoRol);
+	}
+	
+	
 	
 	// ROL
 	@Override
@@ -44,14 +52,32 @@ public class AdminServiceImpl implements AdminService{
 	
 		return adminFeign.listarRol();
 	}
-			
-		
+
+	@Override
+	public ResponseEntity<RolDTO> registrarRol(RolDTO rol) {
+		return adminFeign.registrarRol(rol);
+	}
+	
+	
+	@Override
+	public ResponseEntity<RolDTO> actualizarRol(String idRol, RolDTO rol) {
+		return adminFeign.actualizarRol(idRol, rol);
+	}
+
+	
+	
 	// SUB_MENU
 	@Override
 	public List<SubMenuDTO> listarSubMenu() {
 	
 		return adminFeign.listarSubMenu();
 	}
+	
+	@Override
+	public ResponseEntity<SubMenuDTO> actualizarSubMenu(Long idSubMenus, SubMenuDTO submenu) {
+		return adminFeign.actualizarSubMenu(idSubMenus, submenu);
+	}
+	
 			
 		
 	// TIPO_DOCUMENTO
@@ -66,5 +92,11 @@ public class AdminServiceImpl implements AdminService{
 		
 		return adminFeign.listarUsuariosSistema();
 	}
+	
+	
+	@Override
+    public Map<String, Object> login(String email, String contrasenia) {
+      return adminFeign.login(email, contrasenia);
+    }
 
 }
