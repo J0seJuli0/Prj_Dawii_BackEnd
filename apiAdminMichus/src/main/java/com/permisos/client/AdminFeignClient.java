256b9117f9420f3aec6b9e8d768c9a2b99dab76a
MauriciOjeda <mauricioojeda0204@gmail.com>
2024-12-11 18:17:01 -0500

    Actualizar Cliente ADMIN

--- apiAdminMichus/src/main/java/com/permisos/client/AdminFeignClient.java
+++ apiAdminMichus/src/main/java/com/permisos/client/AdminFeignClient.java
@@ -1,11 +1,16 @@
 package com.permisos.client;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.permisos.modal.dto.*;
 
@@ -16,21 +21,44 @@ public interface AdminFeignClient {
 	@GetMapping("/empleado/listar")
 	public List<EmpleadoDTO> listarEmpleado();
 	
+	
 	// MENU
 	@GetMapping("/menu/listar")
 	public List<MenuDTO> listarMenu();
 
+	
 	// PERMISOS_ROL
 	@GetMapping("/permisosRol/listar")
 	public List<PermisosRolDTO> listarPermisosRol();
+	
+	
+	@PostMapping("/permisosRol/registrar")
+	ResponseEntity<String> registrarPermisoRol(@RequestBody PermisosRolDTO permisoRol);
+	
 	
 	// ROL
 	@GetMapping("/rol/listar")
 	public List<RolDTO> listarRol();
 	
+	@PostMapping("/rol/registrar")
+    ResponseEntity<RolDTO> registrarRol(@RequestBody RolDTO rol);
+	
+	@GetMapping("/rol/obtener/{idRol}")
+    ResponseEntity<RolDTO> obtenerRolPorId(@PathVariable("idRol") String idRol);
+	
+	@PutMapping("/rol/actualizar/{idRol}")
+    ResponseEntity<RolDTO> actualizarRol(@PathVariable("idRol") String idRol, @RequestBody RolDTO rol);
+	
+	
 	// SUB_MENUS
 	@GetMapping("/subMenus/listar")
 	public List<SubMenuDTO> listarSubMenu();
+	
+	@PutMapping("/subMenus/actualizar/{idSubMenus}")
+    ResponseEntity<SubMenuDTO> actualizarSubMenu(
+            @PathVariable("idSubMenus") Long idSubMenus, 
+            @RequestBody SubMenuDTO submenu);
+	
 	
 	// TIPO_DOCUMENTO
 	@GetMapping("/tipoDocumento/listar")
@@ -39,4 +67,13 @@ public interface AdminFeignClient {
 	// USUARIOSSISTEMA
 	@GetMapping("/usuariosSistema/listar")
 	public List<UsuariosSistemaDTO> listarUsuariosSistema();
-}
+	
+
+	
+
+	// LOGIN_
+	@PostMapping("/api/login")
+    Map<String, Object> login(@RequestParam("email") String email, 
+                              @RequestParam("contrasenia") String contrasenia);
+	}
+
