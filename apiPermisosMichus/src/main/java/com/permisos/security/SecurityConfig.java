34a1ca8c01655df6b9fed2afa3ae75c2c33f4a7f
MauriciOjeda <mauricioojeda0204@gmail.com>
2024-12-17 21:55:25 -0500

    Adición de demás vistas

--- apiPermisosMichus/src/main/java/com/permisos/security/SecurityConfig.java
+++ apiPermisosMichus/src/main/java/com/permisos/security/SecurityConfig.java
@@ -27,6 +27,25 @@ public class SecurityConfig {
                 .authorizeHttpRequests(http -> {
                     http.requestMatchers("/api/usuario/login").permitAll();
                     http.requestMatchers("/empleado/listar").hasRole("Jefe");
+                    
+                    http.requestMatchers("/menu/listar").hasRole("Jefe");
+                    
+                    http.requestMatchers("/permisosRol/listar").hasRole("Jefe");
+                    http.requestMatchers("/permisosRol/registrar").hasRole("Jefe");
+                    http.requestMatchers("/permisosRol/obtener/{idRol}/{idSubMenu}").hasRole("Jefe");
+                    
+                    http.requestMatchers("/rol/listar").hasRole("Jefe");
+                    http.requestMatchers("/rol/registrar").hasRole("Jefe");
+                    http.requestMatchers("/rol/obtener/{id}").hasRole("Jefe");
+                    
+                    http.requestMatchers("/subMenus/listar").hasRole("Jefe");
+                    http.requestMatchers("/subMenus/actualizar/{idSubMenus}").hasRole("Jefe");
+                    http.requestMatchers("/subMenus/listarPorMenu/{idMenu}").hasRole("Jefe");
+                    
+                    http.requestMatchers("/tipoDocumento/listar").hasRole("Jefe");
+                    
+                    http.requestMatchers("/usuarios/actualizar-rol").hasRole("Jefe");
+
                     http.anyRequest().authenticated();
                 })
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);  
