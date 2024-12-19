be35529c8770ec5c289348bc038191bd9ff48289
MauriciOjeda <mauricioojeda0204@gmail.com>
2024-12-18 21:51:17 -0500

    Crear EmpleadoDTO Form

---
+++ apiAdminMichus/src/main/java/com/permisos/modal/dto/EmpleadoDTOForm.java
@@ -0,0 +1,14 @@
+package com.permisos.modal.dto;
+
+public record EmpleadoDTOForm(
+		String nombres,
+	    String apellidos,
+	    String documento,
+	    String numDoc,
+	    String fechaIngreso,
+	    String estado,
+	    String rol
+		
+		) {
+
+}
