5295bce20df1d85f32deff2f62996c5b90088950
MauriciOjeda <mauricioojeda0204@gmail.com>
2024-12-18 20:07:14 -0500

    ULTIMA ACTUALIZACION CLIENTE FEIGN

--- apiAdminMichus/src/main/java/com/permisos/client/AdminFeignClient.java
+++ apiAdminMichus/src/main/java/com/permisos/client/AdminFeignClient.java
@@ -1,6 +1,7 @@
 package com.permisos.client;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-import com.permisos.modal.dto.*;
+import com.permisos.modal.dto.EmpleadoDTO;
+import com.permisos.modal.dto.LoginInputDTO;
+import com.permisos.modal.dto.LoginOutputDTO;
+import com.permisos.modal.dto.MenuDTO;
+import com.permisos.modal.dto.PermisosRolDTO;
+import com.permisos.modal.dto.RolDTO;
+import com.permisos.modal.dto.SubMenuDTO;
+import com.permisos.modal.dto.TipoDocumentoDTO;
 
 @FeignClient(name="apiPermisosMichus")
 public interface AdminFeignClient {
@@ -21,24 +29,22 @@ public interface AdminFeignClient {
 	public List<EmpleadoDTO> listarEmpleado(@RequestHeader("Authorization") String authorizationHeader);
 	
 	
+	
 	// MENU
 	@GetMapping("/menu/listar")
 	public List<MenuDTO> listarMenu(@RequestHeader("Authorization") String authorizationHeader);
+	
 	
 	
 	// PERMISOS_ROL
 	@GetMapping("/permisosRol/listar")
 	List<PermisosRolDTO> listarPermisosRol(@RequestHeader("Authorization") String authorizationHeader);
 	
-	
 	@PostMapping("/permisosRol/registrar")
 	ResponseEntity<String> registrarPermisoRol(@RequestBody PermisosRolDTO permisoRol, @RequestHeader("Authorization") String authorizationHeader);	
 	
-	
-	
 	@GetMapping("/permisosRol/obtener/{idRol}/{idSubMenu}")
 	PermisosRolDTO obtenerPermisosPorId(@PathVariable("idRol") String idRol, @PathVariable("idSubMenu") Long idSubMenu, @RequestHeader("Authorization") String authorizationHeader);
-	
 	
 	
 	
@@ -49,26 +55,22 @@ public interface AdminFeignClient {
 	@PostMapping("/rol/registrar")
     ResponseEntity<RolDTO> registrarRol(@RequestBody RolDTO rol, @RequestHeader("Authorization") String authorizationHeader);
 	
-	@GetMapping("rol/obtener/{id}")
+	@GetMapping("/rol/obtener/{id}")
 	RolDTO obtenerRolPorId(@PathVariable("id") String id, @RequestHeader("Authorization") String authorizationHeader);
-	
-	
-	
+
 	
 	
 	// SUB_MENUS
 	@GetMapping("/subMenus/listar")
 	public List<SubMenuDTO> listarSubMenu(@RequestHeader("Authorization") String authorizationHeader);
 	
-	
-	
-	// -------------- NO FUNCIONA --------------
 	@PutMapping("/subMenus/actualizar/{idSubMenus}")
-    ResponseEntity<String> actualizarSubMenu(@PathVariable("idSubMenus") Long idSubMenus, @RequestBody SubMenuDTO submenu, @RequestHeader("Authorization") String authorizationHeader);
+    ResponseEntity<String> actualizarSubMenu(@PathVariable Long idSubMenus, @RequestBody SubMenuDTO submenu, @RequestHeader("Authorization") String authorizationHeader);
+	
+	@GetMapping("/subMenus/listarPorMenu/{idMenu}")
+	List<SubMenuDTO> listarSubMenusPorMenu(@PathVariable("idMenu") Long idMenu, @RequestHeader("Authorization") String authorizationHeader);
+	
 
-	
-	
-	
 	
 	// TIPO_DOCUMENTO
 	@GetMapping("/tipoDocumento/listar")
@@ -76,6 +78,9 @@ public interface AdminFeignClient {
 		
 	
 	
+	// USUARIO
+	@PutMapping("/usuarios/actualizar-rol")
+    ResponseEntity<String> actualizarUsuario(@RequestBody Map<String, String> request, @RequestHeader("Authorization") String authorizationHeader);
 	
 
 
@@ -83,10 +88,3 @@ public interface AdminFeignClient {
 	@PostMapping("/api/usuario/login")
     public LoginOutputDTO login(@RequestBody LoginInputDTO inputDTO);
 	}
-
-
-
-
-
-
-// -------------------------------- 17/12/2024
